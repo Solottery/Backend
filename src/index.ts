a2963592b8c59228d3ec292438a08ebc73557131
Cedric Blaser <cedric.blaser@stud.hslu.ch>
2021-09-25 09:13:48 +0200

    init server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello world!')
+import express from 'express';
+import * as fs from 'fs';
+const fsPromises = fs.promises;
+
+
+// rest of the code remains same
+const app = express();
+const PORT = 42424;
+
+app.get('/tickets/', async (req, res) => {
+    let mintFile = await fsPromises.readFile('mints.json', 'utf-8');
+    if(mintFile){
+        let mints = JSON.parse(mintFile);
+        res.send(mints);
+    }
+});
+
+
+app.listen(PORT, () => {
+    console.log(`[server]: Server is running at https://localhost:${PORT}`);
+});
