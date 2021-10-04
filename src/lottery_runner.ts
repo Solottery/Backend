41a6a4bbd0bc465c842f346718c77a3159892f4b
Solottery <greatodds@tutanota.com>
2021-10-04 21:49:43 +0200

    lottery function is ready

--- src/lottery_runner.ts
+++ src/lottery_runner.ts
@@ -1,11 +1,31 @@
+/**
+ * Includes code form https://github.com/solana-labs/solana-program-library/blob/master/token/js/examples/create_mint_and_transfer_tokens.js
+ * and Metaplex.
+ */
+
 import fs from "fs";
-import {LotteryEntry, LotteryModel} from "./models/LotteryModel";
+import {LotteryEntry, LotteryModel, LotteryResult, WinningAssets} from "./models/LotteryModel";
 import {LotteryTicket} from "./models/lottery-ticket";
-const fsPromises = fs.promises;
 import crypto from "crypto";
-import arrayShuffle from 'array-shuffle';
 import {Keypair, PublicKey} from "@solana/web3.js";
 import {web3} from "@project-serum/anchor";
+import {Token, TOKEN_PROGRAM_ID} from "@solana/spl-token";
+
+const fsPromises = fs.promises;
+
+
+const NETWORK = "devnet";
+
+const shuffleArray = (array: any[]): any => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
 
 // Thanks @metaplex
 export function loadWalletKey(keypair: string): Keypair {
@@ -14,7 +34,7 @@ export function loadWalletKey(keypair: string): Keypair {
     );
 }
 
-const sendSol = async (amount: number, winningTicket: LotteryEntry) =>  {
+const sendSol = async (amount: number, winningTicket: LotteryEntry): Promise<string> => {
     const jackPotWallet = loadWalletKey('./lottery_data/jackpot_wallet.json')
 
     let transaction = new web3.Transaction().add(
@@ -24,7 +44,7 @@ const sendSol = async (amount: number, winningTicket: LotteryEntry) =>  {
             lamports: web3.LAMPORTS_PER_SOL * amount,
         }),
     );
-    const solConnection = new web3.Connection(web3.clusterApiUrl('mainnet-beta'));
+    const solConnection = new web3.Connection(web3.clusterApiUrl(NETWORK));
 
     // Sign transaction, broadcast, and confirm
     let signature = await web3.sendAndConfirmTransaction(
@@ -32,12 +52,60 @@ const sendSol = async (amount: number, winningTicket: LotteryEntry) =>  {
         transaction,
         [jackPotWallet],
     );
+
+    return signature;
+}
+
+
+const sendSPLToken = async (tokenAddress: string, winningTicket: LotteryEntry, amount: number): Promise<string> => {
+    try {
+
+        const jackPotWallet = loadWalletKey('./lottery_data/jackpot_wallet.json')
+        const winner = new PublicKey(winningTicket.owner);
+        const solConnection = new web3.Connection(web3.clusterApiUrl(NETWORK));
+
+        //get the token account of the toWallet Solana address, if it does not exist, create it
+        const token = new Token(solConnection, new PublicKey(tokenAddress), TOKEN_PROGRAM_ID, jackPotWallet);
+        const toTokenAccount = await token.getOrCreateAssociatedAccountInfo(
+            winner,
+        );
+        const fromTokenAccount = await token.getOrCreateAssociatedAccountInfo(
+            jackPotWallet.publicKey,
+        );
+        const signature = await token.transfer(fromTokenAccount.address, toTokenAccount.address, jackPotWallet, [], web3.LAMPORTS_PER_SOL * amount);
+
+        // // Add token transfer instructions to transaction
+        // const transaction = new web3.Transaction().add(
+        //     Token.createTransferInstruction(
+        //         TOKEN_PROGRAM_ID,
+        //         fromTokenAccount.address,
+        //         toTokenAccount.address,
+        //         winner,
+        //         [],
+        //         amount,
+        //     ),
+        // );
+        //
+        //
+        // // Sign transaction, broadcast, and confirm
+        // const signature = await web3.sendAndConfirmTransaction(
+        //     solConnection,
+        //     transaction,
+        //     [jackPotWallet],
+        //     {commitment: 'confirmed'},
+        // );
+
+        return signature;
+    } catch (e) {
+        console.log(e);
+        return "";
+    }
 }
 
 const run_lottery = async () => {
     let lotteriesData = await fsPromises.readFile('./lottery_data/lotteries.json', 'utf-8');
-    if(lotteriesData){
-        const lotteries = JSON.parse(lotteriesData) as LotteryModel[];
+    if (lotteriesData) {
+        const lotteries = JSON.parse(lotteriesData).lotteries as LotteryModel[];
         const runningLotteries = lotteries.filter(l => !l.finished)
         runningLotteries.sort((l1, l2) => {
             if (l1.time < l2.time) {
@@ -50,21 +118,23 @@ const run_lottery = async () => {
             return 0;
         });
 
+        const lottery_data_path = './lottery_data/' + runningLotteries[0].id + "/";
         // if its time for the snapshot
-        if(runningLotteries[0].time <= (new Date(Date.now() - 1000 * 60))){
-            await fsPromises.copyFile('mints_details.json', '/lottery_data/' + runningLotteries[0].id);
+        if (new Date(runningLotteries[0].time) <= (new Date(Date.now() - 1000 * 60))) {
+            await fsPromises.mkdir(lottery_data_path);
+            await fsPromises.copyFile('mints_details.json', lottery_data_path + 'mints_details.json');
         }
 
         // lottery time
-        if(runningLotteries[0].time <= new Date()){
-            let mintFile = await fsPromises.readFile('mints_details.json', 'utf-8');
-            if(mintFile){
+        if (new Date(runningLotteries[0].time) <= new Date()) {
+            let mintFile = await fsPromises.readFile(lottery_data_path + 'mints_details.json', 'utf-8');
+            if (mintFile) {
                 const tickets = JSON.parse(mintFile) as LotteryTicket[];
                 const lotteryList = [];
                 // create lottery list with tickets
-                for(let ticket in tickets){
+                for (let ticket in tickets) {
                     let ticketInfo = tickets[ticket]
-                    for(let i = 0; i++; i < Number(ticketInfo.playMultiplier.value)){
+                    for (let i = 0; i < Number(ticketInfo.playMultiplier.value); i++) {
                         lotteryList.push({
                             owner: ticketInfo.owner,
                             ticket: ticketInfo.mint,
@@ -74,13 +144,52 @@ const run_lottery = async () => {
                 }
 
                 // shuffle list with random number
-                const randomList = arrayShuffle(lotteryList);
+                const randomList = shuffleArray(lotteryList);
                 const winningIndex = crypto.randomInt(0, randomList.length - 1);
                 const winningTicket = randomList[winningIndex];
 
+                const winnings = runningLotteries[0].assets;
+
+                const winAssets: WinningAssets[] = [];
+                for (let i in winnings) {
+                    const win = winnings[i];
+                    if (win.sol) {
+                        const amount = Number(winningTicket.winMultiplier) * Number(win.amount);
+                        const sign = await sendSol(amount, winningTicket);
+                        winAssets.push({
+                            website: win.website,
+                            amount: amount,
+                            name: win.name,
+                            tx: sign
+                        } as WinningAssets);
+                    } else if (win.nft) {
+                        const sign = await sendSPLToken(win.address, winningTicket, 1);
+                        winAssets.push({
+                            website: win.website,
+                            amount: 1,
+                            name: win.name,
+                            tx: sign
+                        } as WinningAssets);
+                    } else {
+                        const amount = Number(winningTicket.winMultiplier) * Number(win.amount);
+                        const sign = await sendSPLToken(win.address, winningTicket, amount);
+                        winAssets.push({
+                            website: win.website,
+                            amount: amount,
+                            name: win.name,
+                            tx: sign
+                        } as WinningAssets);
+                    }
+                }
 
 
+                const result = {
+                    winner: winningTicket.owner,
+                    ticket: winningTicket.ticket,
+                    assets: winAssets
+                } as LotteryResult;
 
+                fs.writeFileSync(lottery_data_path + 'winner.json', JSON.stringify(result))
             }
         }
 
@@ -88,3 +197,4 @@ const run_lottery = async () => {
 }
 
 
+run_lottery().then(r => console.log("fertig"));
--- src/models/LotteryModel.ts
+++ src/models/LotteryModel.ts
@@ -2,19 +2,35 @@
 export interface LotteryModel {
     id: number;
     name: string;
-    time: Date;
+    time: string;
     assets: AssetModel[];
     finished: boolean;
 }
 
 export interface AssetModel {
     address: string;
-    amount: string;
-    isSol: boolean;
+    amount: number;
+    sol: boolean;
+    nft: boolean;
+    name: string;
+    website: string;
 }
 
 export interface LotteryEntry {
     owner: string;
     ticket: string;
     winMultiplier: number;
+}
+
+export interface LotteryResult {
+    winner: string;
+    ticket: string;
+    assets: []
+}
+
+export interface WinningAssets {
+    name: string;
+    tx: string;
+    amount: number;
+    website: string;
 }
