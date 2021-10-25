99139dfa1eeb3f5516611e07c657c95ff6a3dc13
Solottery <greatodds@tutanota.com>
2021-10-25 22:13:08 +0200

    added new give away

--- src/get_nft_info_simple.ts
+++ src/get_nft_info_simple.ts
@@ -1,7 +1,7 @@
 import {getMetaDataFromMint} from "./helpers/nft";
 import {web3} from "@project-serum/anchor";
 
-const nftAddress = '811VuVpZAX3SCyXb8nCSQjkv1tPgKqcaWrXJz8piJiek';
+const nftAddress = '5a99k6oyPhdJmxRLfccQUsTChJ6biXgKWhANf7vzsrQV';
 const solConnection = new web3.Connection(web3.clusterApiUrl('mainnet-beta'));
 
 const getInfo = async () => {
